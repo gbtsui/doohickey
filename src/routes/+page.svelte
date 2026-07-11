1278988f54a58e0a7b854e9e67c070ffbb2abc05
gbtsui <gabriel.tsui@protonmail.com>
2026-07-11 16:56:52 -0400

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+
+import {resolve} from "$app/paths";
+</script>
+
+<h1>doohickey</h1>
+<h2>random stuff.</h2>
+
+<div>
+    <div>directory</div>
+    <a href={resolve("/why")}>why</a>
+</div>
