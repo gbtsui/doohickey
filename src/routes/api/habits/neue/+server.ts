1f2f88ae896265f9dd6fd5ec936c10fe5696aa46
gbtsui <gabriel.tsui@protonmail.com>
2026-07-17 15:14:47 -0400

    feat: how the hell do HTTP requests work? still trying to figure it out. i kinda have zod going now tho

--- src/routes/api/habits/neue/+server.ts
+++ src/routes/api/habits/neue/+server.ts
@@ -1,8 +1,11 @@
 //neue deutsche
 
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
+import * as database from '$lib/server/db';
+import { habit } from '$lib/server/db/schema';
+import { HabitSchema } from '$lib/server/zod/schema';
 
-export const POST = (req) => {
+export const POST = async ({ request }) => {
 	//alle varten auf das licht
 	/*
 	request stuff:
@@ -10,7 +13,48 @@ export const POST = (req) => {
 
 	 */
 
+	//const {name, description, weeklyGoal} = await request.json()
+	const reqBody = await request.json();
+	//const {name, description, weeklyGoal} = await HabitSchema.parseAsync(reqBody);
+	const zodResult =  HabitSchema.safeParse(reqBody);
 
+	if (!zodResult.success) {
+		return json(
+			{
+				success: false,
+				errors: zodResult.error
+			},
+			{ status: 400 }
+		);
+	}
 
-	return json("lowk idk what im doing yet")
-}
+	const { name, description, weeklyGoal } = zodResult.data;
+
+	console.log(name, description, weeklyGoal);
+
+	try {
+		const [newHabit] = await database.db
+			.insert(habit)
+			.values({
+				name,
+				description,
+				weeklyGoal
+			})
+			.returning();
+
+		return json(
+			{
+				success: true,
+				data: newHabit
+			},
+			{ status: 201 }
+		);
+	} catch (e) {
+		console.error(e);
+		if (e instanceof Error) {
+			return error(500, e.message);
+		} else {
+			return error(500);
+		}
+	}
+};
