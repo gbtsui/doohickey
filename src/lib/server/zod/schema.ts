5eb7e57c358f747520aaf18030b11e85580c64ca
gbtsui <gabriel.tsui@protonmail.com>
2026-07-17 15:16:45 -0400

    fix: i can coerce now nvm

--- src/lib/server/zod/schema.ts
+++ src/lib/server/zod/schema.ts
@@ -3,6 +3,6 @@ import z from 'zod';
 export const HabitSchema = z.object({
 	name: z.string().min(1, "gotta name yo habit gng").max(100),
 	description: z.string().max(500),
-	weeklyGoal: z.number().int().min(0).max(7)
+	weeklyGoal: z.coerce.number().int().min(0).max(7)
 })
 
