719410199233389a058b263439a0c1a79f4a3663
gbtsui <gabriel.tsui@protonmail.com>
2026-07-18 18:50:03 -0400

    feat: SLASH COMMAND FINALLY WORKS LETS GOOOOOO

--- src/lib/slack/app.ts
+++ src/lib/slack/app.ts
@@ -17,11 +17,23 @@ slack.message("hello", async ({ message, say }) => {
 
 const API = "https://slack.com/api"
 
+import {SLACK_BOT_TOKEN} from "$env/static/private"
+
 export const slackFetch = async (endpoint: string, body: object) => {
+	/*
+	console.log(process.env.SLACK_BOT_TOKEN)
+
+	console.log("token exists:", !!process.env.SLACK_BOT_TOKEN)
+	console.log("token prefix:", process.env.SLACK_BOT_TOKEN?.slice(0, 10))
+	
+	 */
+
+	console.log(SLACK_BOT_TOKEN)
 	const response = await fetch(`${API}/${endpoint}`, {
 		method: "POST",
 		headers: {
-			Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+			//Authorization: `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+			Authorization: `Bearer ${SLACK_BOT_TOKEN}`,
 			"Content-Type": "application/json"
 		},
 		body: JSON.stringify(body)
@@ -30,23 +42,32 @@ export const slackFetch = async (endpoint: string, body: object) => {
 	const result = await response.json()
 
 	if (!result.ok) {
-		throw new Error(result.error)
+		throw new Error(result.error);
 	}
 
-	return result
+	return new Response(result.body)
+	//return result
 }
 
-export const handleEvent = async (event: object) => {
+export const handleEvent = async (event: FormData) => {
 	//sigh idk what to do just yet
 
-	return new Response("OK")
+	console.log("channel_name: ", event.get("channel_name"));
+	console.log("command: ", event.get("command"));
+	console.log("user_id: ", event.get("user_id"));
+
+	return sendMessage(event.get("channel_id") as string, "hi lol")
+	//return new Response("OK")
 }
 
+/*
 export const handleMessage = (event: object) => {
 	if (event.text === "ping"){
 		return sendMessage(event.channel, "pong")
 	}
 }
+
+ */
 
 export const sendMessage = (channel: string, text: string) =>  {
 	return slackFetch("chat.postMessage", {
