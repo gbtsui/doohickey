d3215775f02ded999066825d4265928ce46233a1
gbtsui <gabriel.tsui@protonmail.com>
2026-07-19 20:39:41 -0400

    feat: YES I CAN FINALLY WRITE MY HABITS RAGHGHGGHGHGHHHH

--- src/routes/api/slack/+server.ts
+++ src/routes/api/slack/+server.ts
@@ -1,6 +1,9 @@
 //import { handleEvent } from '$lib/slack/app';
 
 import { MEMBER_ID } from '$env/static/private';
+import * as database from "$lib/server/db"
+import { habitEntry } from '$lib/server/db/schema';
+import { slackFetch } from '$lib/slack/app';
 
 export const POST = async ({ request }) => {
 	//verify signature hnnnnnnng
@@ -43,7 +46,7 @@ export const POST = async ({ request }) => {
 			const selectedHabits: Array<{ id: string; name: string }> = [];
 
 			console.log("---BRRRR LOGS STARTING BRRRR---")
-			Object.entries(payload.state.values).forEach(([blockId, blockState]: [string, any]) => {
+			Object.entries(payload.state.values).forEach(([, blockState]: [string, any]) => {
 				Object.entries(blockState).forEach(([actionId, actionState]: [string, any]) => {
 					if (actionId === 'habit_checkbox' && actionState.selected_options) {
 						console.log("habit checkbox id")
@@ -58,8 +61,61 @@ export const POST = async ({ request }) => {
 			});
 
 			console.log("selected habits", selectedHabits)
+
+			if (selectedHabits.length > 0) {
+				try {
+//					const now = new Date()
+//forgot that i have auto-now lol
+//					const insertValues = selectedHabits.map((habit: {id: string, name: string}) => {habitId: habit.id})
+
+					const insertValues = selectedHabits.map((habit) => {return {habit_id: habit.id}})
+					await database.db.insert(habitEntry).values(insertValues)
+					console.log(`tracked ${selectedHabits.length} jawns`)
+
+				} catch (e) {
+					console.error("failed to save ts. ", e)
+
+					await fetch(response_url, {
+						method: 'POST',
+						headers: { 'Content-Type': 'application/json' },
+						body: JSON.stringify({
+							text: "failure",
+							response_type: "ephemeral"
+						})
+					});
+
+					return new Response(null, { status: 200 });
+				}
+			}
+
+			const habitNames = selectedHabits.map(h => h.name).join(', ') || 'n/a (lazy chud)';
+
+			//the ting goes
+			await slackFetch("chat.update", {
+				channel: channel.id,
+				ts: message.ts,
+				text: "tracked yo habits habibi",
+				blocks: [
+					{
+						type: "section",
+						text: {
+							type: "mrkdwn",
+							text: `including: ${habitNames}`
+						}
+					},
+					{
+						type: "context",
+						elements: [
+							{
+								type: "mrkdwn",
+								text: `@ ${new Date().toLocaleString()}`
+							}
+						]
+					}
+				]
+			})
 		}
 	}
 
-	return new Response('ok');
+	return new Response('OK');
 };
