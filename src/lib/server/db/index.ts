9cf9d0ac1df921f397a9e8c1228b42a58e4c7289
gbtsui <gabriel.tsui@protonmail.com>
2026-07-30 15:12:02 -0400

    hotfix: use transaction pooling

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -5,6 +5,6 @@ import { DATABASE_URL } from '$env/static/private';
 
 if (!DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = postgres(DATABASE_URL);
+const client = postgres(DATABASE_URL, {prepare: false});
 
 export const db = drizzle(client, { schema });
