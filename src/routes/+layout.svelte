3e56f06da0bc83945f917782cafb397054c69955
gbtsui <gabriel.tsui@protonmail.com>
2026-07-30 14:39:20 -0400

    fix: forgot how layouts work, added a navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
     import "@fontsource/noto-serif"
+    import {resolve} from "$app/paths";
 
 
     let { children } = $props();
@@ -14,4 +15,7 @@
 </style>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<nav class="w-[95vw] h-[3rem] flex flex-row mx-[2.5vw] my-[0.5rem] px-[1rem] items-center bg-stone-200">
+    <a href={resolve("/")} class="hover:text-lg cursor-pointer transition-all">home</a>
+</nav>
 {@render children()}
